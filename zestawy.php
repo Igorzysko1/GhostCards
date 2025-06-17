8d33b03accf13bbce3b208968a39afa9c36e358e
Igor <igormusial08@gmail.com>
2025-06-17 19:49:48 +0200

    Add deleting logic

--- zestawy.php
+++ zestawy.php
@@ -156,6 +156,32 @@ if (!isset($_GET['action'])) {
             }
         break;
 
+    case "delete":
+        if (!isset($_SESSION['uzytkownik_id'])) {
+            header("Location: logowanie.php");
+        } else {
+            if (!isset($_GET['zestaw_id'])) {
+                echo "<p class='error-text'>Nie wybrano zestawu!</p>";
+            } else {
+                $zestaw_id = $_GET['zestaw_id'];
+                $q_zestawData = $conn->query("SELECT * FROM zestawy WHERE zestaw_id = '$zestaw_id';");
+                
+                if ($q_zestawData->num_rows == 0) {
+                    echo "<p class='error-text'>Nie wybrano poprawnego zestawu!</p>";
+                } else {
+                    $row_zestawData = $q_zestawData->fetch_assoc();
+                    $uzytkownik_id = $_SESSION['uzytkownik_id'];
+                    
+                    if ($row_zestawData['uzytkownik_id'] != $uzytkownik_id) {
+                        echo "<p class='error-text'>Zestaw nie jest twój!</p>";
+                    } else {
+                        $conn->query("DELETE FROM zestawy WHERE zestaw_id = '$zestaw_id';");
+                        header("Location: index.php");
+                    }
+                }
+            }
+        }
+        break;
     }
 }
 
