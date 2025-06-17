bdedf02fe844b80a48cff8ffa8f9898b3f0dc168
mxkolvj <mikolaj_kurowski@outlook.com>
2025-06-17 13:00:22 +0200

    Add db_connection file and include it in header

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,10 @@
+<?php require_once 'db_connection.php'; ?>
+
 <!DOCTYPE html>
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ghost School</title>
+    <title>GhostCards</title>
 </head>
 <body>
-    
