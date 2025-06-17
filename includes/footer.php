489740a4307b452c81bdf6b20436b0e33dd294c1
mxkolvj <mikolaj_kurowski@outlook.com>
2025-06-17 14:42:00 +0200

    Add header footer styles and html

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
     <footer>
         <section class="footer-end">
             <span class="authors">Aplikacja wykonana przez: Igor Musiał, Mikołaj Kurowski</span>
-            <a href="https://github.com/Igorzysko1/GhostSchool" class="github-link">Repo GitHub</a>
+            <a href="https://github.com/Igorzysko1/GhostSchool" class="github-link"><i class="fa fa-github"></i></a>
             <!-- TODO: logo mini -->
         </section>
     </footer>
--- includes/header.php
+++ includes/header.php
@@ -9,13 +9,14 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
     <header>
         <section class="header-data">
             <!-- logo -->
-            <span class="app-name">GhostCards</span>
+            <span class="app-name"><span class="letter-highlight">G</span>host<span class="letter-highlight">C</span>ards</span>
         </section>
         <nav class="header-nav">
             <a href="index.php" class="header-button">Strona Główna</a>
