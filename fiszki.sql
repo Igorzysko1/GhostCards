9add4cfedbbca9f7d77061b113bd8106c50294a9
Igor <igor.musial@uczen.ckziu.net>
2025-06-17 13:01:01 +0200

    Add database

---
+++ fiszki.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Cze 17, 2025 at 12:59 PM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ghostschool`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `fiszki`
+--
+
+CREATE TABLE `fiszki` (
+  `fiszka_id` int(11) NOT NULL,
+  `pytanie` text NOT NULL,
+  `odpowiedz` varchar(255) NOT NULL,
+  `ostatnio_wyswietlone` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `fiszki`
+--
+ALTER TABLE `fiszki`
+  ADD PRIMARY KEY (`fiszka_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `fiszki`
+--
+ALTER TABLE `fiszki`
+  MODIFY `fiszka_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
