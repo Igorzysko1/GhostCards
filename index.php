14762c8a7fac2541267869ee4bc2084202e9ee14
Igor <igormusial08@gmail.com>
2025-06-17 19:14:04 +0200

    Add additional functions when you are owner of certain zestaw

--- index.php
+++ index.php
@@ -8,8 +8,20 @@ echo "<div class='fiszkas-list'>";
 
 while ($a_selectZestaw = $q_selectZestawy->fetch_assoc()) {
     echo "<div class='fiszka-set'>";
-    echo "<p class='fiszka-set-name'>".$a_selectZestaw['nazwa']."</p>";
+    if (isset($_SESSION['uzytkownik_id'])) {
+        $uzytkownik_id = $_SESSION['uzytkownik_id'];
+        if ($a_selectZestaw['uzytkownik_id'] == $uzytkownik_id) {
+            echo "<p class='fiszka-set-name'>".$a_selectZestaw['nazwa']." (Twój)</p>";
+        } else {
+            echo "<p class='fiszka-set-name'>".$a_selectZestaw['nazwa']."</p>";
+        }
+    }
     echo "<a href='zestawy.php?action=view&zestaw_id=".$a_selectZestaw['zestaw_id']."' class='view-fiszka'><i class='fa fa-eye'></i>&nbsp;&nbsp;Zobacz zestaw</a>";
+    if (isset($_SESSION['uzytkownik_id'])) {
+        if ($a_selectZestaw['uzytkownik_id'] == $uzytkownik_id) {
+            echo "<a href='zestawy.php?action=edit&zestaw_id=".$a_selectZestaw['zestaw_id']."' class='view-fiszka'>Zmień nazwę zestawu</a>";
+        }
+    }
     echo "</div>";
 }
 echo "</div>";
