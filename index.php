409eb023c251d327432c991367b3edaaba17a3df
Igor <igormusial08@gmail.com>
2025-06-17 13:16:58 +0200

    Add proper displaying fiszkas

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
+<?php include "includes/header.php"; ?>
 <a href="fiszki.php?action=add">Dodaj fiszkę</a>
-<a href="fiszki.php?action=edit&fiszka_id">Dodaj fiszkę</a>
+<h1>Fiszki:</h1>
+<?php
+$q_selectFiszkas = $conn->query("SELECT * FROM fiszki;");
+
+while ($a_selectFiszka = $q_selectFiszkas->fetch_assoc()) {
+    echo "<p>";
+    echo "<h3>".$a_selectFiszka['pytanie']."</h3>";
+    echo "<a href='fiszki.php?action=edit&fiszka_id=".$a_selectFiszka['fiszka_id']."'>Edytuj fiszkę</a>";
+    echo "</p>";
+}
+?>
